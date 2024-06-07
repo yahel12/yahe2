69939bf2831e0fd3f7762c96e326d64ed115e723
blealex <98812061+blealex@users.noreply.github.com>
2024-06-07 18:30:48 +0100

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -28,8 +28,8 @@ async def answer(bot, query):
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='okDa',
-                           switch_pm_parameter="hehe")
+                           switch_pm_text='hey comrade',
+                           switch_pm_parameter="how are you?")
         return
 
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
