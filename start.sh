dd7e1e33d7f4cde8211a3bd80029173961be3b0b
blealex <98812061+blealex@users.noreply.github.com>
2024-06-08 04:46:27 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQ
 fi
-cd /DQTheFileDonorBot
+cd /DQ
 pip3 install -U -r requirements.txt
 echo "Starting DQ...."
 python3 bot.py
