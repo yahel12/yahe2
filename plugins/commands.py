3ee5d096bfe41f41216f01e942165052c3dd0368
blealex <98812061+blealex@users.noreply.github.com>
2024-06-08 04:34:57 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -307,7 +307,7 @@ async def start(client, message):
     if CUSTOM_FILE_CAPTION:
         try:
             f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
-        ept Exception as e:
+        except Exception as e:
             logger.exception(e)
             f_caption=f_caption
     if f_caption is None:
