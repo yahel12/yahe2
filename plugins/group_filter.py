760c6c35d88f3724e2f32e210d021478d7f17825
Godstime315 <97334195+Godstime315@users.noreply.github.com>
2024-06-15 13:31:50 +0100

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -105,3 +105,389 @@ async def next_page(bot, query):
     
     await query.answer()
 
+
+async def auto_filter(client, msg, spoll=False):
+    if not spoll:
+        message = msg
+        settings = await get_settings(message.chat.id)
+        if message.text.startswith("/"): return  # ignore commands
+        if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
+            return
+        if len(message.text) < 100:
+            search = message.text
+            files, offset, total_results = await get_search_results(message.chat.id, search.lower(), offset=0, filter=True)
+            if not files:
+                if settings["spell_check"]:
+                    return await advantage_spell_chok(client, msg)
+                return
+        else:
+            return
+    else:
+        message = msg.message.reply_to_message  # msg will be callback query
+        search, files, offset, total_results = spoll
+        settings = await get_settings(message.chat.id)
+
+    temp.KEYWORD[message.from_user.id] = search
+
+    pre = 'filep' if settings['file_secure'] else 'file'
+
+    if settings["button"]:
+        btn = [
+            [
+                InlineKeyboardButton(
+                    text=f"➲ {get_size(file.file_size)} || {file.file_name}",
+                    callback_data=f'{pre}#{file.file_id}'
+                ),
+            ]
+            for file in files
+        ]
+    else:
+        btn = [
+            [
+                InlineKeyboardButton(
+                    text=f"{file.file_name}",
+                    callback_data=f'{pre}#{file.file_id}',
+                ),
+                InlineKeyboardButton(
+                    text=f"{get_size(file.file_size)}",
+                    callback_data=f'{pre}#{file.file_id}',
+                ),
+            ]
+            for file in files
+        ]
+
+    try:
+        btn.insert(0, [InlineKeyboardButton(text="🔞 CLICK HERE FOR OUR ADULT CHANNEL", url='https://t.me/Adultship_films')])
+    except KeyError:
+        await save_group_settings(message.chat.id, 'auto_delete', True)
+        btn.insert(0, [InlineKeyboardButton(text="🔞 CLICK HERE FOR OUR ADULT CHANNEL", url='https://t.me/Adultship_films')])
+
+    if offset:
+        key = f"{message.chat.id}-{message.id}"
+        BUTTONS[key] = search
+        req = message.from_user.id if message.from_user else 0
+        try:
+            btn.append([
+                InlineKeyboardButton("📑 ᴩᴀɢᴇꜱ", callback_data="pages"),
+                InlineKeyboardButton(text=f"1/{math.ceil(int(total_results) / (10 if settings.get('max_btn') else int(MAX_B_TN)))}", callback_data="pages"),
+                InlineKeyboardButton(text="𝕹𝖊𝖝𝖙 »»", callback_data=f"next_{req}_{key}_{offset}")
+            ])
+        except KeyError:
+            await save_group_settings(message.chat.id, 'max_btn', True)
+            btn.append([
+                InlineKeyboardButton("📑 ᴩᴀɢᴇꜱ", callback_data="pages"),
+                InlineKeyboardButton(text=f"1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
+                InlineKeyboardButton(text="𝕹𝖊𝖝𝖙 »»", callback_data=f"next_{req}_{key}_{offset}")
+            ])
+    else:
+        btn.append([
+            InlineKeyboardButton(text="𝐍𝐎 𝐌𝐎𝐑𝐄 𝐏𝐀𝐆𝐄𝐒 𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄", callback_data="pages")
+        ])
+
+    imdb = await get_poster(search, file=files[0].file_name) if settings["imdb"] else None
+    TEMPLATE = settings['template']
+    cap = TEMPLATE.format(
+        query=search,
+        title=imdb['title'],
+        votes=imdb['votes'],
+        aka=imdb["aka"],
+        seasons=imdb["seasons"],
+        box_office=imdb['box_office'],
+        localized_title=imdb['localized_title'],
+        kind=imdb['kind'],
+        imdb_id=imdb["imdb_id"],
+        cast=imdb["cast"],
+        runtime=imdb["runtime"],
+        countries=imdb["countries"],
+        certificates=imdb["certificates"],
+        languages=imdb["languages"],
+        director=imdb["director"],
+        writer=imdb["writer"],
+        producer=imdb["producer"],
+        composer=imdb["composer"],
+        cinematographer=imdb["cinematographer"],
+        music_team=imdb["music_team"],
+        distributors=imdb["distributors"],
+        release_date=imdb['release_date'],
+        year=imdb['year'],
+        genres=imdb['genres'],
+        poster=imdb['poster'],
+        plot=imdb['plot'],
+        rating=imdb['rating'],
+        url=imdb['url'],
+        **locals()
+    ) if imdb else f"<code>{search}</code>"
+
+    try:
+        if imdb and imdb.get('poster'):
+            hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+            await handle_auto_delete(hehe, message, settings)
+        else:
+            fuk = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+            await handle_auto_delete(fuk, message, settings)
+    except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+        poster = imdb.get('poster').replace('.jpg', "._V1_UX360.jpg") if imdb and imdb.get('poster') else None
+        if poster:
+            hmm = await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+            await handle_auto_delete(hmm, message, settings)
+        else:
+            fek = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+            await handle_auto_delete(fek, message, settings)
+    except Exception as e:
+        logger.exception(e)
+        fek = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+        await handle_auto_delete(fek, message, settings)
+
+    if spoll:
+        await msg.message.delete()
+
+async def handle_auto_delete(msg, original_msg, settings):
+    try:
+        if settings['auto_delete']:
+            await asyncio.sleep(180)
+            await msg.delete()
+            await original_msg.delete()
+    except KeyError:
+        await save_group_settings(original_msg.chat.id, 'auto_delete', True)
+        await asyncio.sleep(180)
+        await msg.delete()
+        await original_msg.delete()
+
+
+async def advantage_spell_chok(client, msg):
+    mv_id = msg.id
+    mv_rqst = msg.text
+    settings = await get_settings(msg.chat.id)
+    query = re.sub(
+        r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
+        "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
+    query = query.strip() + " movie"
+    try:
+        movies = await get_poster(mv_rqst, bulk=True)
+    except Exception as e:
+        logger.exception(e)
+        reqst_gle = mv_rqst.replace(" ", "+")
+        button = [[
+            InlineKeyboardButton("Gᴏᴏɢʟᴇ", url=f"https://www.google.com/search?q={reqst_gle}")
+        ]]
+        k = await msg.reply(
+            script.I_CUDNT.format(mv_rqst),
+            reply_markup=InlineKeyboardMarkup(button)
+        )
+        await asyncio.sleep(30)
+        await k.delete()
+        return
+    movielist = []
+    if not movies:
+        reqst_gle = mv_rqst.replace(" ", "+")
+        button = [[
+            InlineKeyboardButton("Gᴏᴏɢʟᴇ", url=f"https://www.google.com/search?q={reqst_gle}")
+        ]]
+        k = await msg.reply(
+            script.I_CUDNT.format(mv_rqst),
+            reply_markup=InlineKeyboardMarkup(button)
+        )
+        await asyncio.sleep(30)
+        await k.delete()
+        return
+    movielist += [movie.get('title') for movie in movies]
+    movielist += [f"{movie.get('title')} {movie.get('year')}" for movie in movies]
+    SPELL_CHECK[mv_id] = movielist
+    btn = [
+        [
+            InlineKeyboardButton(
+                text=movie_name.strip(),
+                callback_data=f"spol#{mv_id}#{k}",
+            )
+        ]
+        for k, movie_name in enumerate(movielist)
+    ]
+    btn.append([InlineKeyboardButton(text="Close", callback_data=f'spol#{mv_id}#close_spellcheck')])
+    spell_check_del = await msg.reply(
+        script.CUDNT_FND.format(mv_rqst),
+        reply_markup=InlineKeyboardMarkup(btn)
+    )
+    try:
+        if settings['auto_delete']:
+            await asyncio.sleep(30)
+            await spell_check_del.delete()
+    except KeyError:
+        grpid = await active_connection(str(msg.from_user.id))
+        await save_group_settings(grpid, 'auto_delete', True)
+        settings = await get_settings(msg.chat.id)
+        if settings['auto_delete']:
+            await asyncio.sleep(30)
+            await spell_check_del.delete()
+
+
+async def manual_filters(client, message, text=False):
+    settings = await get_settings(message.chat.id)
+    group_id = message.chat.id
+    name = text or message.text
+    reply_id = message.reply_to_message.id if message.reply_to_message else message.id
+    keywords = await get_filters(group_id)
+    for keyword in reversed(sorted(keywords, key=len)):
+        pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
+        if re.search(pattern, name, flags=re.IGNORECASE):
+            reply_text, btn, alert, fileid = await find_filter(group_id, keyword)
+
+            if reply_text:
+                reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t")
+
+            try:
+                if fileid == "None":
+                    await send_manual_message(client, group_id, reply_text, btn, settings, reply_id, message)
+                else:
+                    await send_manual_media(client, message, group_id, reply_text, btn, fileid, settings, reply_id)
+            except Exception as e:
+                logger.exception(e)
+            break
+    else:
+        return False
+
+async def send_manual_message(client, group_id, reply_text, btn, settings, reply_id, message):
+    if btn == "[]":
+        joelkb = await client.send_message(
+            group_id,
+            reply_text,
+            disable_web_page_preview=True,
+            protect_content=True if settings["file_secure"] else False,
+            reply_to_message_id=reply_id
+        )
+    else:
+        button = eval(btn)
+        joelkb = await client.send_message(
+            group_id,
+            reply_text,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(button),
+            protect_content=True if settings["file_secure"] else False,
+            reply_to_message_id=reply_id
+        )
+    await handle_manual_auto_filter(client, message, joelkb, settings)
+
+async def send_manual_media(client, message, group_id, reply_text, btn, fileid, settings, reply_id):
+    if btn == "[]":
+        joelkb = await client.send_cached_media(
+            group_id,
+            fileid,
+            caption=reply_text or "",
+            protect_content=True if settings["file_secure"] else False,
+            reply_to_message_id=reply_id
+        )
+    else:
+        button = eval(btn)
+        joelkb = await message.reply_cached_media(
+            fileid,
+            caption=reply_text or "",
+            reply_markup=InlineKeyboardMarkup(button),
+            reply_to_message_id=reply_id
+        )
+    await handle_manual_auto_filter(client, message, joelkb, settings)
+
+async def handle_manual_auto_filter(client, message, joelkb, settings):
+    try:
+        if settings['auto_ffilter']:
+            await auto_filter(client, message)
+            await handle_manual_auto_delete(joelkb, settings)
+        else:
+            await handle_manual_auto_delete(joelkb, settings)
+    except KeyError:
+        grpid = await active_connection(str(message.from_user.id))
+        await save_group_settings(grpid, 'auto_ffilter', True)
+        settings = await get_settings(message.chat.id)
+        if settings['auto_ffilter']:
+            await auto_filter(client, message)
+        await handle_manual_auto_delete(joelkb, settings)
+
+async def handle_manual_auto_delete(joelkb, settings):
+    if settings.get('auto_delete', False):
+        await asyncio.sleep(180)
+        await joelkb.delete()
+
+
+async def global_filters(client, message, text=False):
+    settings = await get_settings(message.chat.id)
+    group_id = message.chat.id
+    name = text or message.text
+    reply_id = message.reply_to_message.id if message.reply_to_message else message.id
+    keywords = await get_gfilters('gfilters')
+    for keyword in reversed(sorted(keywords, key=len)):
+        pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
+        if re.search(pattern, name, flags=re.IGNORECASE):
+            reply_text, btn, alert, fileid = await find_gfilter('gfilters', keyword)
+
+            if reply_text:
+                reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t")
+
+            try:
+                if fileid == "None":
+                    await send_global_message(client, group_id, reply_text, btn, settings, reply_id, message)
+                else:
+                    await send_global_media(client, message, group_id, reply_text, btn, fileid, settings, reply_id)
+            except Exception as e:
+                logger.exception(e)
+            break
+    else:
+        return False
+
+async def send_global_message(client, group_id, reply_text, btn, settings, reply_id, message):
+    if btn == "[]":
+        joelkb = await client.send_message(
+            group_id,
+            reply_text,
+            disable_web_page_preview=True,
+            reply_to_message_id=reply_id
+        )
+    else:
+        button = eval(btn)
+        joelkb = await client.send_message(
+            group_id,
+            reply_text,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(button),
+            reply_to_message_id=reply_id
+        )
+    await handle_global_auto_filter(client, message, joelkb, settings)
+
+async def send_global_media(client, message, group_id, reply_text, btn, fileid, settings, reply_id):
+    if btn == "[]":
+        joelkb = await client.send_cached_media(
+            group_id,
+            fileid,
+            caption=reply_text or "",
+            reply_to_message_id=reply_id
+        )
+    else:
+        button = eval(btn)
+        joelkb = await message.reply_cached_media(
+            fileid,
+            caption=reply_text or "",
+            reply_markup=InlineKeyboardMarkup(button),
+            reply_to_message_id=reply_id
+        )
+    await handle_global_auto_filter(client, message, joelkb, settings)
+
+async def handle_global_auto_filter(client, message, joelkb, settings):
+    manual = await manual_filters(client, message)
+    if not manual:
+        try:
+            if settings['auto_ffilter']:
+                await auto_filter(client, message)
+                await handle_global_auto_delete(joelkb, settings)
+            else:
+                await handle_global_auto_delete(joelkb, settings)
+        except KeyError:
+            grpid = await active_connection(str(message.from_user.id))
+            await save_group_settings(grpid, 'auto_ffilter', True)
+            settings = await get_settings(message.chat.id)
+            if settings['auto_ffilter']:
+                await auto_filter(client, message)
+            await handle_global_auto_delete(joelkb, settings)
+    else:
+        await handle_global_auto_delete(joelkb, settings)
+
+async def handle_global_auto_delete(joelkb, settings):
+    if settings.get('auto_delete', False):
+        await asyncio.sleep(180)
+        await joelkb.delete()
