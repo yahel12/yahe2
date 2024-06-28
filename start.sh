36d20edd7a8b63ca568665c5c677a3d6dbcc4b8c
Godstime5 <119031870+Godstime5@users.noreply.github.com>
2024-06-28 20:23:45 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hislordshipb/Hislordshipb.git /DQ
+  git clone https://github.com/Godstime5/hey_tess.git /hey_tess
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Hislordshipb
+  git clone $UPSTREAM_REPO /hey_tess
 fi
-cd /Hislordshipb
+cd /hey_tess
 pip3 install -U -r requirements.txt
-echo "Starting Hislordshipb...."
+echo "Starting hey_tess...."
 python3 bot.py
