457c42e13db4c5a414ec7f77299ddabaa0e2efe1
Godstime5 <119031870+Godstime5@users.noreply.github.com>
2024-06-28 17:16:21 +0100

    Update filters_mdb.py

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -1,11 +1,13 @@
 import pymongo
-from info import DATABASE_URI, DATABASE_NAME, SECONDDB_URI
 from pyrogram import enums
-import logging
+from info import DATABASE_URI, DATABASE_NAME, SECONDDB_URI
 from sample_info import tempDict
+import logging
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
+# Initialize MongoDB clients and databases
 myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 
@@ -13,107 +15,115 @@ myclient2 = pymongo.MongoClient(SECONDDB_URI)
 mydb2 = myclient2[DATABASE_NAME]
 
 async def add_filter(grp_id, text, reply_text, btn, file, alert):
+    """
+    Add or update a filter in the appropriate MongoDB collection based on tempDict['indexDB'].
+    """
     if tempDict['indexDB'] == DATABASE_URI:
         mycol = mydb[str(grp_id)]
     else:
         mycol = mydb2[str(grp_id)]
-    # mycol.create_index([('text', 'text')])
 
     data = {
-        'text':str(text),
-        'reply':str(reply_text),
-        'btn':str(btn),
-        'file':str(file),
-        'alert':str(alert)
+        'text': str(text),
+        'reply': str(reply_text),
+        'btn': str(btn),
+        'file': str(file),
+        'alert': str(alert)
     }
 
     try:
         mycol.update_one({'text': str(text)},  {"$set": data}, upsert=True)
-    except:
-        logger.exception('Some error occured!', exc_info=True)
-             
-     
+    except Exception:
+        logger.exception('Error occurred while adding/updating filter!', exc_info=True)
+
+
 async def find_filter(group_id, name):
+    """
+    Find a filter by name in the appropriate MongoDB collection.
+    """
     mycol = mydb[str(group_id)]
     mycol2 = mydb2[str(group_id)]
-    
-    query = mycol.find( {"text":name})
+
+    query = mycol.find({"text": name})
     query2 = mycol2.find({"text": name})
-    # query = mycol.find( { "$text": {"$search": name}})
+
     try:
         for file in query:
             reply_text = file['reply']
             btn = file['btn']
             fileid = file['file']
-            try:
-                alert = file['alert']
-            except:
-                alert = None
+            alert = file.get('alert')  # Use .get() to safely retrieve 'alert'
         return reply_text, btn, alert, fileid
-    except:
+    except Exception:
         try:
             for file in query2:
                 reply_text = file['reply']
                 btn = file['btn']
                 fileid = file['file']
-                try:
-                    alert = file['alert']
-                except:
-                    alert = None
+                alert = file.get('alert')
             return reply_text, btn, alert, fileid
-        except:
+        except Exception:
             return None, None, None, None
 
 
 async def get_filters(group_id):
+    """
+    Retrieve all filters from both MongoDB collections for a given group_id.
+    """
     mycol = mydb[str(group_id)]
     mycol2 = mydb2[str(group_id)]
 
     texts = []
-    query = mycol.find()
-    query2 = mycol2.find()
     try:
-        for file in query:
+        for file in mycol.find():
             text = file['text']
             texts.append(text)
-    except:
+    except Exception:
         pass
+
     try:
-        for file in query2:
+        for file in mycol2.find():
             text = file['text']
             texts.append(text)
-    except:
+    except Exception:
         pass
+
     return texts
 
 
 async def delete_filter(message, text, group_id):
+    """
+    Delete a filter by text from the appropriate MongoDB collection.
+    """
     mycol = mydb[str(group_id)]
     mycol2 = mydb2[str(group_id)]
-    
-    myquery = {'text':text }
+
+    myquery = {'text': text}
     query = mycol.count_documents(myquery)
     query2 = mycol2.count_documents(myquery)
+
     if query == 1:
         mycol.delete_one(myquery)
         await message.reply_text(
-            f"'`{text}`'  deleted. I'll not respond to that filter anymore.",
+            f"'{text}' deleted. I'll not respond to that filter anymore.",
+            quote=True,
+            parse_mode=enums.ParseMode.MARKDOWN
+        )
+    elif query2 == 1:
+        mycol2.delete_one(myquery)
+        await message.reply_text(
+            f"'{text}' deleted. I'll not respond to that filter anymore.",
             quote=True,
             parse_mode=enums.ParseMode.MARKDOWN
         )
     else:
-        if query2 == 1:
-            mycol2.delete_one(myquery)
-            await message.reply_text(
-                f"'`{text}`'  deleted. I'll not respond to that filter anymore.",
-                quote=True,
-                parse_mode=enums.ParseMode.MARKDOWN
-            )
-        else:
-            await message.reply_text("Couldn't find that filter!", quote=True)
+        await message.reply_text("Couldn't find that filter!", quote=True)
 
 
 async def del_all(message, group_id, title):
+    """
+    Delete all filters from both MongoDB collections for a given group_id.
+    """
     if str(group_id) not in mydb.list_collection_names() and str(group_id) not in mydb2.list_collection_names():
         await message.edit_text(f"Nothing to remove in {title}!")
         return
@@ -123,40 +133,44 @@ async def del_all(message, group_id, title):
     try:
         mycol.drop()
         mycol2.drop()
-        await message.edit_text(f"All filters from {title} has been removed")
-    except:
+        await message.edit_text(f"All filters from {title} have been removed")
+    except Exception:
         await message.edit_text("Couldn't remove all filters from group!")
-        return
 
 
 async def count_filters(group_id):
+    """
+    Count the total number of filters in both MongoDB collections for a given group_id.
+    """
     mycol = mydb[str(group_id)]
     mycol2 = mydb2[str(group_id)]
 
-    count = (mycol.count())+(mycol2.count())
-    return False if count == 0 else count
+    count = mycol.count_documents({}) + mycol2.count_documents({})
+    return count if count != 0 else False
 
 
 async def filter_stats():
+    """
+    Retrieve statistics about filters from both MongoDB collections.
+    """
     collections = mydb.list_collection_names()
     collections2 = mydb2.list_collection_names()
 
+    # Remove special collections if present
     if "CONNECTION" in collections:
         collections.remove("CONNECTION")
     elif "CONNECTION" in collections2:
         collections2.remove("CONNECTION")
 
-    totalcount = 0
+    total_count = 0
+
+    # Calculate total count of filters across all collections
     for collection in collections:
-        mycol = mydb[collection]
-        count = mycol.count()
-        totalcount += count
+        total_count += mydb[collection].count_documents({})
 
     for collection in collections2:
-        mycol2 = mydb2[collection]
-        count2 = mycol2.count()
-        totalcount += count2
+        total_count += mydb2[collection].count_documents({})
 
-    totalcollections = len(collections)+len(collections2)
+    total_collections = len(collections) + len(collections2)
 
-    return totalcollections, totalcount
+    return total_collections, total_count
