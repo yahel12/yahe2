5006928a8bd87eb9f3a55da12a82b71e7a3bd268
Godstime5 <119031870+Godstime5@users.noreply.github.com>
2024-06-28 18:53:35 +0100

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -98,7 +98,7 @@ async def save_file(media):
             mime_type=media.mime_type,
             caption=media.caption.html if media.caption else None,
         )
-        
+    
         # Commit file to the chosen database
         await file.commit()
     except DuplicateKeyError:
@@ -112,167 +112,158 @@ async def save_file(media):
         return True, 1
 
 async def get_search_results(chat_id, query, file_type=None, max_results=10, offset=0, filter=False):
-    """For given query, return (results, next_offset, total_results)."""
-    
-    # Adjust max_results based on group settings if chat_id is provided
+    """For given query return (results, next_offset)"""
     if chat_id is not None:
+        settings = await get_settings(int(chat_id))
         try:
-            settings = await get_settings(int(chat_id))
-            max_btn = settings.get('max_btn', False)
-            max_results = 10 if max_btn else int(max_btn)
+            if settings['max_btn']:
+                max_results = 10
+            else:
+                max_results = int(MAX_B_TN)
         except KeyError:
             await save_group_settings(int(chat_id), 'max_btn', False)
             settings = await get_settings(int(chat_id))
-            max_btn = settings.get('max_btn', False)
-            max_results = 10 if max_btn else int(max_btn)
-
+            if settings['max_btn']:
+                max_results = 10
+            else:
+                max_results = int(MAX_B_TN)
     query = query.strip()
-    
-    # Construct regular expression pattern based on query
-    raw_pattern = (
-        r'.' if not query else  # Match anything if query is empty
-        rf'(\b|[.\+\-_]){re.escape(query)}(\b|[.\+\-_])' if ' ' not in query else  # Match exact words
-        query.replace(' ', r'.*[\s\.\+\-_()]')  # Match words with spaces replaced by any separator
-    )
-
-    try:
-        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
-    except re.error:
-        logger.exception('Invalid regular expression pattern')
-        return [], '', 0
-
-    # Build the filter query
-    filter_query = {'$or': [{'file_name': regex}]}
-    if filter and USE_CAPTION_FILTER:
-        filter_query['$or'].append({'caption': regex})
-    if file_type:
-        filter_query['file_type'] = file_type
-
-    # Calculate total results by summing counts from both databases
-    total_results = await Media.count_documents(filter_query) + await Media2.count_documents(filter_query)
-
-    # Ensure max_results is even
-    if max_results % 2 != 0:
-        logger.info(f"Since max_results is an odd number ({max_results}), bot will use {max_results + 1} as max_results to make it even.")
-        max_results += 1
-
-    # Fetch results from both collections
-    cursor1 = Media.find(filter_query).sort('$natural', -1).skip(offset).limit(max_results)
-    cursor2 = Media2.find(filter_query).sort('$natural', -1).skip(offset).limit(max_results)
-    
-    # Combine the results
-    files1 = await cursor1.to_list(length=max_results)
-    files2 = await cursor2.to_list(length=max_results)
-    files = files1[:max_results // 2] + files2[:max_results // 2]
-
-    # Calculate next offset
-    next_offset = offset + len(files)
-    if next_offset >= total_results:
-        next_offset = ''  # Reset next_offset if it exceeds total results
-
-    return files, next_offset, total_results
-
-async def get_bad_files(query: str, file_type: str = None, filter: bool = False):
-    """For given query, return (results, total_results)."""
-    query = query.strip()
-
+    #if filter:
+        #better ?
+        #query = query.replace(' ', r'(\s|\.|\+|\-|_)')
+        #raw_pattern = r'(\s|_|\-|\.|\+)' + query + r'(\s|_|\-|\.|\+)'
     if not query:
         raw_pattern = '.'
     elif ' ' not in query:
         raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
     else:
         raw_pattern = query.replace(' ', r'.*[\s\.\+\-_()]')
-
+    
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
-    except re.error:
-        logger.exception('Invalid regular expression pattern')
+    except:
         return []
 
-    filter_query = {'$or': [{'file_name': regex}]}
     if USE_CAPTION_FILTER:
-        filter_query['$or'].append({'caption': regex})
+        filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
+    else:
+        filter = {'file_name': regex}
 
     if file_type:
-        filter_query['file_type'] = file_type
+        filter['file_type'] = file_type
 
-    # Querying both collections and merging results
-    cursor1 = Media.find(filter_query).sort('$natural', -1)
-    cursor2 = Media2.find(filter_query).sort('$natural', -1)
+    total_results = ((await Media.count_documents(filter))+(await Media2.count_documents(filter)))
+
+    #verifies max_results is an even number or not
+    if max_results%2 != 0: #if max_results is an odd number, add 1 to make it an even number
+        logger.info(f"Since max_results is an odd number ({max_results}), bot will use {max_results+1} as max_results to make it even.")
+        max_results += 1
+
+    cursor = Media.find(filter)
+    cursor2 = Media2.find(filter)
+    # Sort by recent
+    cursor.sort('$natural', -1)
+    cursor2.sort('$natural', -1)
+    # Slice files according to offset and max results
+    cursor2.skip(offset).limit(max_results)
+    # Get list of files
+    fileList2 = await cursor2.to_list(length=max_results)
+    if len(fileList2)<max_results:
+        next_offset = offset+len(fileList2)
+        cursorSkipper = (next_offset-(await Media2.count_documents(filter)))
+        cursor.skip(cursorSkipper if cursorSkipper>=0 else 0).limit(max_results-len(fileList2))
+        fileList1 = await cursor.to_list(length=(max_results-len(fileList2)))
+        files = fileList2+fileList1
+        next_offset = next_offset + len(fileList1)
+    else:
+        files = fileList2
+        next_offset = offset + max_results
+    if next_offset >= total_results:
+        next_offset = ''
+    return files, next_offset, total_results
+
+async def get_bad_files(query, file_type=None, filter=False):
+    """For given query return (results, next_offset)"""
+    query = query.strip()
+    #if filter:
+        #better ?
+        #query = query.replace(' ', r'(\s|\.|\+|\-|_)')
+        #raw_pattern = r'(\s|_|\-|\.|\+)' + query + r'(\s|_|\-|\.|\+)'
+    if not query:
+        raw_pattern = '.'
+    elif ' ' not in query:
+        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
+    else:
+        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_()]')
     
-    files1 = await cursor1.to_list(length=await Media.count_documents(filter_query))
-    files2 = await cursor2.to_list(length=await Media2.count_documents(filter_query))
-    files = files1 + files2
+    try:
+        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
+    except:
+        return []
 
+    if USE_CAPTION_FILTER:
+        filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
+    else:
+        filter = {'file_name': regex}
+
+    if file_type:
+        filter['file_type'] = file_type
+
+    cursor = Media.find(filter)
+    cursor2 = Media2.find(filter)
+    # Sort by recent
+    cursor.sort('$natural', -1)
+    cursor2.sort('$natural', -1)
+    # Get list of files
+    files = ((await cursor2.to_list(length=(await Media2.count_documents(filter))))+(await cursor.to_list(length=(await Media.count_documents(filter)))))
+
+    #calculate total results
     total_results = len(files)
 
     return files, total_results
 
 async def get_file_details(query):
-    """Fetch file details from MongoDB based on file_id."""
-    filter_query = {'file_id': query}  # Define filter based on file_id query
-
-    # Attempt to find file details in the primary MongoDB collection (Media)
-    filedetails = await Media.find_one(filter_query)
-
-    # If file details are not found in the primary collection, try the secondary collection (Media2)
+    filter = {'file_id': query}
+    cursor = Media.find(filter)
+    filedetails = await cursor.to_list(length=1)
     if not filedetails:
-        filedetails = await Media2.find_one(filter_query)
-
-    return filedetails  # Return the fetched file details (None if not found)
+        cursor2 = Media2.find(filter)
+        filedetails = await cursor2.to_list(length=1)
+    return filedetails
 
 def encode_file_id(s: bytes) -> str:
-    """Encode bytes to a URL-safe base64 string."""
-    result_bytes = b""
-    consecutive_zeros = 0
+    r = b""
+    n = 0
 
-    for byte in s + bytes([22]) + bytes([4]):
-        if byte == 0:
-            consecutive_zeros += 1
+    for i in s + bytes([22]) + bytes([4]):
+        if i == 0:
+            n += 1
         else:
-            if consecutive_zeros:
-                # Append a zero byte followed by the count of consecutive zeros
-                result_bytes += b"\x00" + bytes([consecutive_zeros])
-                consecutive_zeros = 0
-            
-            result_bytes += bytes([byte])  # Append the current byte to the result
+            if n:
+                r += b"\x00" + bytes([n])
+                n = 0
 
-    # Encode the result bytes using URL-safe base64 encoding, decode to UTF-8, and strip trailing '=' characters
-    encoded_str = base64.urlsafe_b64encode(result_bytes).decode().rstrip("=")
-    
-    return encoded_str
-    
+            r += bytes([i])
+
+    return base64.urlsafe_b64encode(r).decode().rstrip("=")
+
+
 def encode_file_ref(file_ref: bytes) -> str:
-    """Encode file reference bytes to a URL-safe base64 string."""
-    # Encode the file_ref using URL-safe base64 encoding
-    encoded = base64.urlsafe_b64encode(file_ref)
-    
-    # Decode the bytes to UTF-8 string and remove trailing '=' characters
-    encoded_str = encoded.decode().rstrip("=")
-    
-    return encoded_str
+    return base64.urlsafe_b64encode(file_ref).decode().rstrip("=")
 
-def unpack_new_file_id(new_file_id: bytes) -> Tuple[bytes, bytes]:
-    """Decode and unpack a new_file_id into file_id and file_ref."""
-    try:
-        # Decoding the new_file_id using a custom function (decode_file_id)
-        decoded = decode_file_id(new_file_id)
-        
-        # Encoding the decoded parts into a new file_id using pack function
-        file_id = encode_file_id(pack("<iiqq",
-                                      int(decoded.file_type),
-                                      decoded.dc_id,
-                                      decoded.media_id,
-                                      decoded.access_hash))
-        
-        # Encoding the file reference part using a custom function (encode_file_ref)
-        file_ref = encode_file_ref(decoded.file_reference)
-        
-        # Returning the encoded file_id and file_ref as a tuple
-        return file_id, file_ref
-    
-    except Exception as e:
-        # Handling exceptions that might occur during decoding or encoding
-        print(f"Error decoding new_file_id: {e}")
-        return b'', b''  # Returning empty bytes objects or handle the error as needed
+
+def unpack_new_file_id(new_file_id):
+    """Return file_id, file_ref"""
+    decoded = FileId.decode(new_file_id)
+    file_id = encode_file_id(
+        pack(
+            "<iiqq",
+            int(decoded.file_type),
+            decoded.dc_id,
+            decoded.media_id,
+            decoded.access_hash
+        )
+    )
+    file_ref = encode_file_ref(decoded.file_reference)
+    return file_id, file_ref
 
