77bd87b93649c4acf4f59ad6044c1c79a62fc81d
Godstime5 <119031870+Godstime5@users.noreply.github.com>
2024-07-07 09:47:23 +0100

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -311,7 +311,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
     elif query.data.startswith("checksub"):
         if AUTH_CHANNEL and not await is_subscribed(client, query):
-            await query.answer("𝖨 𝖫𝗂𝗄𝖾 𝖸𝗈𝗎𝗋 𝖲𝗆𝖺𝗋𝗍𝗇𝖾𝗌𝗌, 𝖡𝗎𝗍 𝖣𝗈𝗇'𝗍 𝖡𝖾 𝖮𝗏𝖾𝗋𝗌𝗆𝖺𝗋𝗍 😏\n𝖩𝗈𝗂𝗇 the 𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅 𝖿𝗂𝗋𝗌𝗍", show_alert=True)
+            await query.answer("Join the 2 Channels pleaseee 🙏 ", show_alert=True)
             return
 
         ident, file_id = query.data.split("#")
